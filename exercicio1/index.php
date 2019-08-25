571908901d54508c480c8b7fb7680a5b2436ab79
Giovane] <giovanelr@gmail.com>
2019-08-25 15:45:59 -0300

    Exercicio 1

---
+++ exercicio1/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *
+ *
+ *
+ Faça um algoritmo em PHP que calcule a soma dos valores do array abaixo. Não é
+permitido utilizar funções já definidas no PHP para realizar a soma, como por
+exemplo array_sum, mas você pode utilizar if, while, for, foreach caso queira.
+$valores = array(1,3,5,9,12,10);
+*
+*
+*
+ */
