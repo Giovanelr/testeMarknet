62d979431593ba30e74aa4d904ddacb17de96556
Giovane] <giovanelr@gmail.com>
2019-08-25 16:22:03 -0300

    Resolicao Exercicio 2

--- exercicio2/index.php
+++ exercicio2/index.php
@@ -10,4 +10,25 @@ automaticamente no momento em que o script for executado.
 *
 *
 *
- */
+*/
+//
+if (empty($_GET['data'])) {
+	echo "Você não inseriu a data.";
+	exit;
+}
+$data = $_GET['data'];
+// Quebra a data para modificar para o formato americano
+$data = explode("/",$data);
+$aux = $data[0];
+$data[0] = $data[2];
+$data[2] = $aux;
+$data = implode("-",$data);
+$data = strtotime($data);
+// Verifica se é maior que a data atual
+// Recebe a data atual já tratada em timestamp
+$data_atual = strtotime(date("Y-m-d"));
+// Calcula a diferença, em dias, das duas datas
+// Tempo em segundos /60 -> tempo em minutos/60 ->tempo em horas/24 -> tempo em dias
+$diferenca = ($data_atual - $data)/60/60/24;
+echo "A diferença entre ".$_GET['data']." e ".date("d/m/Y")." é de :".$diferenca." dias";
+// Alternativa
