25bc4d7c568a2468697dc22fe7c1b23e7a87358c
Giovane] <giovanelr@gmail.com>
2019-08-25 15:56:18 -0300

    Exercicio 2

---
+++ exercicio2/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *
+ *
+ *
+ Faça um algoritmo em PHP que receba uma data através de um parâmetro da url,
+em formato brasileiro (dd/mm/YYYY), e calcule a quantidade de dias passados até a
+data atual. A data atual não deve estar fixa no código, deve-se pegar a data atual
+automaticamente no momento em que o script for executado.
+*
+*
+*
+ */
